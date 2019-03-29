27b88ad8b4592925616555dd69852697cf377b4c
Kostya <lolkazavr1@gmail.com>
2019-03-29 04:59:50 -0700

    README update && task 1

---
+++ lab1/L1.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+function count () {
+    cat /dev/null > log.txt
+    x=0
+    SECONDS=0
+    while [ $SECONDS -ne 60 ]; do
+            sudo tcpdump -c 1 host $1 >> log.txt
+            x=$(wc -l log.txt | awk '{print $1}')
+            if [ $x -eq 100 ]; then
+                 echo "Packet limit!"
+                 sudo iptables -A INPUT -s $1 -j DROP
+                 ((tts = 60 - $SECONDS))
+                 echo "Waiting for $tts seconds"
+                 sleep $tts
+                 sudo iptables -D INPUT -s $1 -j ACCEPT
+                 return 
+            fi    
+    done
+    echo 'time is over'
+    echo "number of captured packets: $x"
+    return
+}
+
+cleanup () { 
+    kill -s SIGTERM $! 
+    exit 0 
+}
+
+if [ $# -eq 0 ]; then
+     echo 'No host address!'
+     exit
+fi
+trap cleanup SIGINT SIGTERM
+while [ 1 ]; do
+    count $1
+done
+
+#Examples:
+#./L1.sh 93.184.220.29
+#./L1.sh 192.168.1.127
+#./L1.sh 217.20.147.1
+
+
